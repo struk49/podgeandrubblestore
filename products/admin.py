6568a5c56dea1f95c76c3c0d7c4893df25a5055c
struk49 <andrewstruk@yahoo.co.uk>
2023-12-12 09:00:47 +0000

    customized admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+# Register your models here
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'price',
+        'discount_price',
+        'sku',
+        'name',
+        'product_description',
+        'gender',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=255)
     friendly_name = models.CharField(max_length=255, null=True, blank=True)
 
