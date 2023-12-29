bf954440aced6a17abdeac57967731243697d560
struk49 <andrewstruk@yahoo.co.uk>
2023-12-29 12:31:57 +0000

    added notifacation css, shopping bag preview and additional messages

--- bag/views.py
+++ bag/views.py
@@ -25,10 +25,12 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-            else:
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
                 bag[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'added size{ size.upper()} { product.name } to your bag')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'added size{ size.upper()} { product.name } to your bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
