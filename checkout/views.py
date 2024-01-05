aae0c0f55d1c811f7099902a23e2c0d1a5220d1c
struk49 <andrewstruk@yahoo.co.uk>
2024-01-05 11:02:34 +0000

    added basic payment functionality

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -17,12 +20,25 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 
+        'Stripe public is missing, did you forget to add one?')
+
     templates = 'checkout/checkout1.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_live_51O2vtYG3G7lKiCY618POHC66h3gueThdXnUmbcyOWX3pJn4uPBXUZwEklFZjAv8Vteo6erMlUPYDvpujVZsrgl0L00VUPQAw6c',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, templates, context)
