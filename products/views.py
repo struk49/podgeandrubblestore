309a8762bdf5c642f9e14319caa4a344e9794bbf
struk49 <andrewstruk@yahoo.co.uk>
2024-01-14 15:12:59 +0000

    added product view, urls and template

--- products/views.py
+++ products/views.py
@@ -1,27 +1,21 @@
-# Code adapted from the CI Boutique Ado mini project
-
-from django.shortcuts import (
-    render, redirect, reverse, get_object_or_404)
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Gender, Category, SubCategory, ProductType
 
+from .models import Product, Category
+from .forms import ProductForm
 
+# Create your views here.
 
 def all_products(request):
-    """
-    A view to show all products, including sorting and search queries
-    """
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+    categories = None
     sort = None
     direction = None
-    query = None
-    gender = None
-    category = None
-    sub_category = None
-    product_type = None
 
     if request.GET:
         if 'sort' in request.GET:
@@ -30,67 +24,34 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-                if sortkey == 'category':
-                    sortkey = 'category__name'
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-        if 'gender' in request.GET:
-            gender = request.GET['gender'].split(',')
-            products = products.filter(
-                gender__name__in=gender)
-            gender = Gender.objects.filter(
-                name__in=gender)
-
+            
         if 'category' in request.GET:
-            category = request.GET['category'].split(',')
-            products = products.filter(
-                category__name__in=category)
-            category = Category.objects.filter(
-                name__in=category)
-
-        if 'sub_category' in request.GET:
-            sub_category = request.GET['sub_category'].split(',')
-            products = products.filter(
-                sub_category__name__in=sub_category)
-            sub_category = SubCategory.objects.filter(
-                name__in=sub_category)
-
-        if 'product_type' in request.GET:
-            product_type = request.GET['product_type'].split(',')
-            products = products.filter(
-                product_type__name__in=product_type)
-            product_type = ProductType.objects.filter(
-                name__in=product_type)
-
-       
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "Please enter a seearch criteria")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
-            queries = (
-                Q(name__icontains=query) |
-                Q(product_description__icontains=query) |
-                Q(gender__name__icontains=query) |
-                Q(category__name__icontains=query) |
-                Q(sub_category__name__icontains=query) |
-                Q(product_type__name__icontains=query))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
-        'current_gender': gender,
-        'current_category': category,
-        'current_sub_category': sub_category,
-        'current_product_type': product_type,
+        'current_categories': categories,
         'current_sorting': current_sorting,
     }
 
@@ -109,3 +70,22 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+def add_product(request):
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
