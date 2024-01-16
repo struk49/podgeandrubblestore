4b00bd24b16076bd534d62b647f2fdc35dee7b6a
struk49 <andrewstruk@yahoo.co.uk>
2024-01-16 13:27:33 +0000

    finished add product function

--- profiles/views.py
+++ profiles/views.py
@@ -6,7 +6,6 @@ from .forms import UserProfileForm
 
 from checkout.models import Order
 
-
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -16,8 +15,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -27,21 +28,21 @@ def profile(request):
         'on_profile_page': True
     }
 
-    return render(request, template, context) 
+    return render(request, template, context)
 
 
 def order_history(request, order_number):
-        order = get_object_or_404(Order, order_number=order_number)
+    order = get_object_or_404(Order, order_number=order_number)
 
-        messages.info(request, (
-            f'This is a past confirmation for order number {order_number}. '
-            'A confirmation email was sent on the order date.'
-        ))
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
 
-        template = 'checkout/checkout_success.html'
-        context = {
-            'order': order,
-            'from_profile': True,
-        }
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
 
-        return render(request, template, context)
+    return render(request, template, context)
