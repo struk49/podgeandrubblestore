d9c6f2fedacf9d40fe5cfb5118ff1db56ff98f11
struk49 <andrewstruk@yahoo.co.uk>
2024-01-22 10:56:19 +0000

    removed secret key and set debug to true

--- podgeandrubble/settings.py
+++ podgeandrubble/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', ')tt9$phf&$6@_j$jsc==pho!l=y7g)pg7ch%2!p7v_xl)866ve')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DECELOPMENT' in os.environ
