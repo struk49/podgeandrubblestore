4dde8436d562e3b648cbfd9e6742ca5c6067a3ab
struk49 <andrewstruk@yahoo.co.uk>
2024-01-23 14:19:04 +0000

    changed host name in setting

--- podgeandrubble/settings.py
+++ podgeandrubble/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DECELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://git.heroku.com/podgeandrubblestore.git', 'localhost']
+ALLOWED_HOSTS = ['podgeandrubblestore.herokuapp.comt', 'localhost']
 
 
 # Application definition
