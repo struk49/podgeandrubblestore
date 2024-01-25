eb8bef962a8c166bc6de1dfe18a96b4985801373
struk49 <andrewstruk@yahoo.co.uk>
2024-01-25 11:46:44 +0000

    deploying part 2

--- podgeandrubble/settings.py
+++ podgeandrubble/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '$=w*t(ml+ya7=ip08_4n517xzvmat-#ff^+#1^5&z^q!-fijmk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DECELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['podgeandrubblestore-a2e07daaf52e.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['podgeandrubbleshop.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -132,6 +132,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
