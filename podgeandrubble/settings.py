06a414a7921994f6af45160ad18aec810169e1d8
struk49 <andrewstruk@yahoo.co.uk>
2024-01-25 12:54:01 +0000

    dployeing to heroku

--- podgeandrubble/settings.py
+++ podgeandrubble/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '$=w*t(ml+ya7=ip08_4n517xzvmat-#ff^+#1^5&z^q!-fijmk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DECELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['podgeandrubbleshop.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['podgeandrubblestore.herokuapp.com', 'localhost']
 
 
 
