d29e1bc7102c99a9d172e215454ff67f5857710f
struk49 <andrewstruk@yahoo.co.uk>
2024-01-25 12:09:44 +0000

    removed secret key

--- podgeandrubble/settings.py
+++ podgeandrubble/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '$=w*t(ml+ya7=ip08_4n517xzvmat-#ff^+#1^5&z^q!-fijmk')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DECELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['podgeandrubbleshop.herokuapp.com', 'localhost']
+
 
 
 # Application definition
