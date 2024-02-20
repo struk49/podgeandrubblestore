1e35370359c8a69c8922a5cb1dc2c7c472466713
struk49 <andrewstruk@yahoo.co.uk>
2024-02-20 14:22:43 +0000

    added heroku url to settings

--- podgeandrubble/settings.py
+++ podgeandrubble/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['podgeandrubbleshop-89a1a769d2d3.herokuapp.com/','localhost', '8000-struk49-podgeandrubbles-x9o6o3p3oof.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['podgeandrubblestore-a2e07daaf52e.herokuapp.com/','localhost', '8000-struk49-podgeandrubbles-x9o6o3p3oof.ws-eu108.gitpod.io']
 
 
 
